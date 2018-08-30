4ee53743b9d036085a7b61ec62f90aadb34ff018
Pepe Valdivia <jvaldivia@softweb.pe>
2018-08-29 23:30:14 -0500

    CRUD de departamentos

--- features/crud_departamentos.feature
+++ features/crud_departamentos.feature
@@ -1,11 +1,15 @@
-Feature: CRUD Paises
+Feature: CRUD Departamentos
   Crear, Editar y Eliminar departamentos
 
-  Scenario: Crear paises
+  Scenario: Crear departamentos
     Given Generar petición HTTP "departamento/guardar" con headers
-    Given Crear POST data crear-departamento
-    Given Crear POST data editar-departamento
-    Given Crear POST data eliminar-departamento
+    #Given Crear POST data crear-departamentos
+    #When Ejecutar petición HTTP
+    #Then Se debe obtener un status code success 200
+    #Given Crear POST data editar-departamentos
+    #When Ejecutar petición HTTP
+    #Then Se debe obtener un status code success 200
+    Given Crear POST data eliminar-departamentos
     When Ejecutar petición HTTP
     Then Se debe obtener un status code success 200
     Then Se debe obtener el id generado
--- features/step_definitions/crud_departamentos.rb
+++ features/step_definitions/crud_departamentos.rb
@@ -1,42 +1,47 @@
-Given("Crear POST data crear-departamento") do
+Given("Crear POST data crear-departamentos") do
   @data = {
     :nuevos => [
       {
-        :id => 'tablaPais_481',
-        :nombre => 'Pais N1',
+        :id => 'tablaDeparamento_481',
+        :nombre => 'Deparamento N1',
       },
       {
-        :id => 'tablaPais_482',
-        :nombre => 'Pais N2',
+        :id => 'tablaDeparamento_482',
+        :nombre => 'Deparamento N2',
       },
     ],
     :editados => [],
     :eliminados => [],
+    :extra => {
+      :pais_id => 2
+    }
   }
 end
 
-Given("Crear POST data editar-departamento") do
+Given("Crear POST data editar-departamentos") do
   @data = {
     :nuevos => [],
     :editados => [
       {
-        :id => 2,
-        :nombre => 'Colombia',
-      },
-      {
-        :id => 3,
-        :nombre => 'Chile',
+        :id => 26,
+        :nombre => 'Colombia XD',
       },
     ],
     :eliminados => [],
+    :extra => {
+      :pais_id => 2
+    }
   }
 end
 
-Given("Crear POST data eliminar-departamento") do
+Given("Crear POST data eliminar-departamentos") do
   @data = {
     :nuevos => [],
     :editados => [],
-    :eliminados => [14,15,16],
+    :eliminados => [26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45],
+    :extra => {
+      :pais_id => 2
+    }
   }
 end
 
